fb5cb288989af0f54dc720abfe17d051aed59a1c
Audrey Gonera <audrey.gonera@gmail.com>
2021-06-09 18:21:34 +0200

    exo 2 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating restaurants..."
+
+Little_italy = { name: "Little Italy", address: "17 place Jacquard Lille", phone_number: "03 88 67 67 56", category: "italian" }
+Chez_la_vieille = { name: "Chez la Vieille", address: "20 rue de Gand Lille ", phone_number: "03 88 67 30 56", category: "italian" }
+Adresse = { name: "L'Adresse'", address: "30 rue des Bouchers", phone_number: "03 98 67 00 56", category: "italian" }
+Comptoir_volant = { name:"Le Comptoir Volant", address: "86 rue de la clé Lille", phone_number: "03 00 67 29 47", category: "italian" }
+Au_Chti_B = { name:"Au Chti B", address: "90 rue Saint André Lille", phone_number: "09 77 55 66 33", category: "italian" }
+
+[Little_italy, Chez_la_vieille, Adresse, Comptoir_volant, Au_Chti_B].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"
